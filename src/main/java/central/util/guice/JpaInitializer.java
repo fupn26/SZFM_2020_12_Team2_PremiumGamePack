ab063ffc8d28fda9e22819f430aa319046679d01
Balázs Bartha <balifej@gmail.com>
2020-11-14 17:06:40 +0100

    JpaInitializer now initializes JPA

--- src/main/java/central/util/guice/JpaInitializer.java
+++ src/main/java/central/util/guice/JpaInitializer.java
@@ -1,4 +1,16 @@
 package central.util.guice;
 
+import com.google.inject.persist.PersistService;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@Singleton
 public class JpaInitializer {
+
+    @Inject
+    public JpaInitializer (PersistService persistService) {
+        persistService.start();
+    }
+
 }
