7c671c8f3cec122c0ea2af79441011f565fd5286
Mezei Botond <botond20@gmail.com>
2020-11-14 19:52:21 +0100

    Added isValidTable method to validate the given table state

--- src/main/java/puckg/state/TableState.java
+++ src/main/java/puckg/state/TableState.java
@@ -14,4 +14,28 @@ public class TableState {
     private Cell[][] table;
 
     private int previousPlayer = 2;
+
+    public boolean isValidTable(int[][] a) {
+        if(a == null || a.length != 6) {
+            return false;
+        }
+        boolean foundBlack = false;
+        for(int[] row : a) {
+            if(row == null || row.length != 6) {
+                return false;
+            }
+            for(int space : row) {
+                if(space < 0 || space >= Cell.values().length) {
+                    return false;
+                }
+                if(space == Cell.BLACK.getValue()) {
+                    if(foundBlack) {
+                        return false;
+                    }
+                    foundBlack = true;
+                }
+            }
+        }
+        return foundBlack;
+    }
 }
