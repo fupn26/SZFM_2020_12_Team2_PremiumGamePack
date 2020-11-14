fe711ff6f7d4f98eb338206fd63062d815a39116
Mezei Botond <botond20@gmail.com>
2020-11-14 19:41:05 +0100

    Added 'of' method to Cell enum class

--- src/main/java/puckg/state/Cell.java
+++ src/main/java/puckg/state/Cell.java
@@ -6,4 +6,11 @@ public enum Cell {
     RED,
     BLUE,
     BLACK;
+
+    public static Cell of(int value) {
+        if (value < 0 || value >= values().length) {
+            throw new IllegalArgumentException();
+        }
+        return values()[value];
+    }
 }
