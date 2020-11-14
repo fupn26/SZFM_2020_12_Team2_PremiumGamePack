e3f2c6d92c653e70c88a0da5aefebc3646ff30ca
Balázs Bartha <balifej@gmail.com>
2020-11-14 17:16:50 +0100

    added imports to central.javafx.controller/StartController.java

--- src/main/java/central/javafx/controller/StartController.java
+++ src/main/java/central/javafx/controller/StartController.java
@@ -1,4 +1,24 @@
 package central.javafx.controller;
 
+import central.javafx.GameApplication;
+import com.jfoenix.controls.JFXButton;
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
 public class StartController {
 }
