af58f1e6783eac875be11b535b340493f85d87d5
Balázs Bartha <balifej@gmail.com>
2020-11-14 17:08:03 +0100

    implemented central.util.guice/PersistenceModule

--- src/main/java/central/util/guice/PersistenceModule.java
+++ src/main/java/central/util/guice/PersistenceModule.java
@@ -1,4 +1,20 @@
 package central.util.guice;
 
-public class PersistenceModule {
+import com.google.inject.AbstractModule;
+import com.google.inject.persist.jpa.JpaPersistModule;
+
+public class PersistenceModule extends AbstractModule {
+
+    private String jpaUnit;
+
+    public PersistenceModule(String jpaUnit) {
+        this.jpaUnit = jpaUnit;
+    }
+
+    @Override
+    protected void configure() {
+        install(new JpaPersistModule(jpaUnit));
+        bind(JpaInitializer.class).asEagerSingleton();
+    }
+
 }
