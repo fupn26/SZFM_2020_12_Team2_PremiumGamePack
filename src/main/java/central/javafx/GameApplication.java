934f62dd02c53d715b8882d9e746722d200ef5b5
Balázs Bartha <balifej@gmail.com>
2020-11-14 17:14:45 +0100

    implemented central.javafx/GameApplication.java

--- src/main/java/central/javafx/GameApplication.java
+++ src/main/java/central/javafx/GameApplication.java
@@ -1,4 +1,21 @@
 package central.javafx;
 
-public class GameApplication {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class GameApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(GameApplication.class.getResource("/fxml/central/start.fxml"));
+        primaryStage.setTitle("Test Project");
+        primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
 }
