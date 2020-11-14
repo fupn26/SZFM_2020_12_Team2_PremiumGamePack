74f3f25f6f697600c06fd36f6d0c57fced7a6771
Mezei Botond <botond20@gmail.com>
2020-11-14 19:47:05 +0100

    Added documentation to Cell enum class

--- src/main/java/puckg/state/Cell.java
+++ src/main/java/puckg/state/Cell.java
@@ -1,12 +1,35 @@
 package puckg.state;
 
+/**
+ * Class representing the empty cell, the black cell, the red and the blue pucks.
+ */
 public enum Cell {
 
+    /**
+     * Enum constant referring to a cell that is empty.
+     */
     EMPTY,
+    /**
+     * Enum constant referring to a cell that contains a red puck.
+     */
     RED,
+    /**
+     * Enum constant referring to a cell that contains a blue puck.
+     */
     BLUE,
+    /**
+     * Enum constant referring to the black cell.
+     */
     BLACK;
 
+    /**
+     * Returns the instance represented by the value specified.
+     *
+     * @param value the value representing the instance
+     * @return the instance represented by the value specified
+     * @throws IllegalArgumentException if the value specified does not
+     * represent an instance
+     */
     public static Cell of(int value) {
         if (value < 0 || value >= values().length) {
             throw new IllegalArgumentException();
@@ -14,10 +37,21 @@ public enum Cell {
         return values()[value];
     }
 
+    /**
+     * Returns
+     * the integer value that represents this instance.
+     *
+     * @return the integer value that represents the instance
+     */
     public int getValue() {
         return ordinal();
     }
 
+    /**
+     * Returns the opposite puck of the given puck.
+     *
+     * @return the opposite puck
+     */
     public Cell opposite() {
         switch (this) {
             case RED: return BLUE;
@@ -26,6 +60,12 @@ public enum Cell {
         throw new AssertionError();
     }
 
+    /**
+     * Returns the string format the specified enum constant's ordinal number.
+     *
+     * @return the string value that represents the specified enum constant's
+     * ordinal number
+     */
     public String toString() {
         return Integer.toString(ordinal());
     }
