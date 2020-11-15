c071f524aa9d76c734667d03a0b824f91f516114
Mezei Botond <botond20@gmail.com>
2020-11-15 14:55:53 +0100

    Added variables to GameController in puckg

--- src/main/java/puckg/controller/GameController.java
+++ src/main/java/puckg/controller/GameController.java
@@ -1,4 +1,62 @@
 package puckg.controller;
 
+import javafx.animation.Timeline;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.GridPane;
+import puckg.state.TableState;
+
+import javax.inject.Inject;
+import java.time.Instant;
+import java.util.List;
+
 public class GameController {
+
+    @Inject
+    private FXMLLoader fxmlLoader;
+
+    private String[] players = new String[2];
+    private int[] points = new int[2];
+    private int player;
+    private TableState tableState;
+    private Instant startTime;
+    private List<Image> cellImages;
+    private List<Image> cellImagesOnMove;
+    private int prevRow = -1;
+    private int prevCol = -1;
+    private int playerGaveUp = -1;
+
+    @FXML
+    private Label infoLabel;
+
+    @FXML
+    private Label player1Label;
+
+    @FXML
+    private Label player2Label;
+
+    @FXML
+    private Label player1PointsLabel;
+
+    @FXML
+    private Label player2PointsLabel;
+
+    @FXML
+    private GridPane gameGrid;
+
+    @FXML
+    private Label stopWatchLabel;
+
+    @FXML
+    private Timeline stopWatchTimeLine;
+
+    @FXML
+    private Button giveUpButton;
+
+    private BooleanProperty gameOver = new SimpleBooleanProperty();
 }
