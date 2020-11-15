38f88a12870c105229814432b9ef2c1f8fce8114
Benedek <szabobenedek99@gmail.com>
2020-11-15 17:21:29 +0100

    added isValidBoard function to SweGameState.java

--- src/main/java/swegame/state/SweGameState.java
+++ src/main/java/swegame/state/SweGameState.java
@@ -23,5 +23,62 @@ public class SweGameState implements Cloneable {
             {1, 2, 1, 2}
     };
 
+    /**
+     * The array storing the current configuration of the board.
+     */
+    @Setter(AccessLevel.NONE)
+    private Cell[][] board;
+
+    private int player = 1;
+
+    /**
+     * Creates a {@code SweGameState} object representing the (original)
+     * initial state of the game.
+     */
+    public SweGameState() {
+        this(INITIAL);
+    }
+
+    /**
+     * Creates a {@code SweGameState} object that is initialized it with
+     * the specified array.
+     *
+     * @param a an array of size 5;4 representing the initial configuration
+     *          of the tray
+     * @throws IllegalArgumentException if the array does not represent a valid
+     *                                  configuration of the tray
+     */
+    public SweGameState(int[][] a) {
+        if (!isValidBoard(a)) {
+            throw new IllegalArgumentException();
+        }
+        initBoard(a);
+    }
+
+    private boolean isValidBoard(int[][] a) {
+        if (a == null || a.length != 5) {
+            return false;
+        }
+        int red = 0, blue = 0;
+        for (int[] row : a) {
+            if (row == null || row.length != 4) {
+                return false;
+            }
+            for (int space : row) {
+                if (space < 0 || space >= Cell.values().length) {
+                    return false;
+                }
+                if (space == Cell.RED.getValue()) {
+                    red++;
+                }
+                if (space == Cell.BLUE.getValue()) {
+                    blue++;
+                }
+            }
+        }
+        return red == 4 && blue == 4;
+
+    }
+
 
 }
