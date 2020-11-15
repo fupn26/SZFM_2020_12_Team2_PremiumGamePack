a18f01028eb7829ae0fc5c953d8039a8561dfa60
Balázs Bartha <balifej@gmail.com>
2020-11-15 13:35:04 +0100

    added necessary imports and annotations to MsweeperState class

--- src/main/java/minesweeper/state/MsweeperState.java
+++ src/main/java/minesweeper/state/MsweeperState.java
@@ -1,4 +1,14 @@
 package minesweeper.state;
 
-public class MsweeperState {
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Random;
+
+/**
+ * Class representing the state of the puzzle.
+ */
+@Data
+@Slf4j
+public class MsweeperState implements Cloneable {
 }
