08a3eac26c13c41edf476878fe593f377fe7ae61
Mezei Botond <botond20@gmail.com>
2020-11-15 14:38:06 +0100

    Added GuiceContext to StartController in puckg

--- src/main/java/puckg/controller/StartController.java
+++ src/main/java/puckg/controller/StartController.java
@@ -1,4 +1,14 @@
 package puckg.controller;
 
+import central.util.guice.PersistenceModule;
+import com.gluonhq.ignite.guice.GuiceContext;
+import com.google.inject.AbstractModule;
+
+import java.util.List;
+
 public class StartController {
+
+    private GuiceContext context = new GuiceContext(this, () -> List.of(
+            new AbstractModule() {}
+    ));
 }
