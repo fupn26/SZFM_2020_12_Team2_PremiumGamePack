6e167b1e912019776b9ef3d489b7b8f18ac300a0
Balázs Bartha <balifej@gmail.com>
2020-11-15 13:35:50 +0100

    declared required attributes of MsweeperState class

--- src/main/java/minesweeper/state/MsweeperState.java
+++ src/main/java/minesweeper/state/MsweeperState.java
@@ -11,4 +11,40 @@ import java.util.Random;
 @Data
 @Slf4j
 public class MsweeperState implements Cloneable {
+
+
+    /**
+     * Random object to generate random numbers.
+     */
+    private Random random = new Random();
+
+    /**
+     * Array representing where the mines are in the grid.
+     */
+    private int[][] minegrid;
+
+    /**
+     * Array representing where the flags are in the grid.
+     */
+    private int[][] flaggrid;
+
+    /**
+     * Array representing which squares are revealed in the grid.
+     */
+    private int[][] revealgrid;
+
+    /**
+     * Array representing the number of mines adjacent to each grid.
+     */
+    private int[][] aroundgrid;
+
+    /**
+     * The number of rows in the grid.
+     */
+    private int rownumber;
+
+    /**
+     * The number of columns in the grid.
+     */
+    private int colnumber;
 }
