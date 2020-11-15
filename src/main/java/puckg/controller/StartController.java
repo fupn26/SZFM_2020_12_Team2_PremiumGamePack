a282a41855483cd8737c6086dfb821386e87060c
Mezei Botond <botond20@gmail.com>
2020-11-15 14:43:37 +0100

    Added necessary variables to StartController in puckg

--- src/main/java/puckg/controller/StartController.java
+++ src/main/java/puckg/controller/StartController.java
@@ -1,9 +1,15 @@
 package puckg.controller;
 
-import central.util.guice.PersistenceModule;
 import com.gluonhq.ignite.guice.GuiceContext;
 import com.google.inject.AbstractModule;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
+import javax.inject.Inject;
 import java.util.List;
 
 public class StartController {
@@ -11,4 +17,25 @@ public class StartController {
     private GuiceContext context = new GuiceContext(this, () -> List.of(
             new AbstractModule() {}
     ));
+
+    @Inject
+    private FXMLLoader fxmlLoader;
+
+    @FXML
+    private TextField player1NameTextField;
+
+    @FXML
+    private TextField player2NameTextField;
+
+    @FXML
+    private Label errorLabel1;
+
+    @FXML
+    private Label errorLabel2;
+
+    @FXML
+    private TextArea ruleArea;
+
+    @FXML
+    private Button rulesButton;
 }
