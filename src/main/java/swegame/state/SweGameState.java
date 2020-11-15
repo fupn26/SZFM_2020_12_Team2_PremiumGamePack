c45c70b5d971e251d3bc8cd481064caa215c53bc
Benedek <szabobenedek99@gmail.com>
2020-11-15 17:15:51 +0100

    edited mistaken names

---
+++ src/main/java/swegame/state/SweGameState.java
@@ -0,0 +1,27 @@
+package swegame.state;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Class representing the state of the game.
+ */
+@Data
+@Slf4j
+public class SweGameState implements Cloneable {
+
+    /**
+     * The array representing the initial configuration of the board.
+     */
+    public static final int[][] INITIAL = {
+            {2, 1, 2, 1},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {1, 2, 1, 2}
+    };
+
+
+}
