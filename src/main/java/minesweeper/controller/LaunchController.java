797bfeaee8ef1589779cbc490416c0a364a4d90d
Balázs Bartha <balifej@gmail.com>
2020-11-15 13:58:34 +0100

    added necessary imports to LaunchController class for Minesweeper

--- src/main/java/minesweeper/controller/LaunchController.java
+++ src/main/java/minesweeper/controller/LaunchController.java
@@ -1,4 +1,24 @@
 package minesweeper.controller;
 
+import central.util.guice.PersistenceModule;
+import com.gluonhq.ignite.guice.GuiceContext;
+import com.google.inject.AbstractModule;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.inject.Inject;
+import java.io.IOException;
+import java.util.List;
+
+
+@Slf4j
 public class LaunchController {
 }
