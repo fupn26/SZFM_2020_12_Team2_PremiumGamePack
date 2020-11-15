e4db2dc851d5503b7aa45d540bfc742b32c80084
Mezei Botond <botond20@gmail.com>
2020-11-15 14:45:15 +0100

    Added startAction function to StartController in puckg

--- src/main/java/puckg/controller/StartController.java
+++ src/main/java/puckg/controller/StartController.java
@@ -2,14 +2,20 @@ package puckg.controller;
 
 import com.gluonhq.ignite.guice.GuiceContext;
 import com.google.inject.AbstractModule;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import javax.inject.Inject;
+import java.io.IOException;
 import java.util.List;
 
 public class StartController {
@@ -43,5 +49,21 @@ public class StartController {
     public void initialize() {
         ruleArea.setVisible(false);
         context.init();
+    }
+
+    public void startAction(ActionEvent actionEvent) throws IOException {
+        if(player1NameTextField.getText().isEmpty()) {
+            errorLabel1.setText("Enter Player1's name!");
+        } else if(player2NameTextField.getText().isEmpty()) {
+            errorLabel2.setText("Enter Player2's name!");
+        } else {
+            fxmlLoader.setLocation(getClass().getResource("/fxml/puckg/game.fxml"));
+            Parent root = fxmlLoader.load();
+            fxmlLoader.<GameController>getController().setPlayer1Name(player1NameTextField.getText());
+            fxmlLoader.<GameController>getController().setPlayer2Name(player2NameTextField.getText());
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        }
     }
 }
