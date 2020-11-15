6875ba19c7f0a602c39d3aff5e064a026a3cadc6
Benedek <szabobenedek99@gmail.com>
2020-11-15 17:03:51 +0100

    added enum to Cell.java

---
+++ src/main/java/triplepuck/state/Cell.java
@@ -0,0 +1,21 @@
+package swegame.state;
+/**
+ * Class representing a cell and its possible content.
+ */
+
+public enum Cell {
+    /**
+     * Represents the empty cell.
+     */
+    EMPTY,
+    /**
+     * Represents a red cell.
+     */
+    RED,
+    /**
+     * Represents a blue cell.
+     */
+    BLUE;
+
+
+}
