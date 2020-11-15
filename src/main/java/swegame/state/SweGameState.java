0ef0c836c4e5c506fa776a7f39f02c26108aa349
Benedek <szabobenedek99@gmail.com>
2020-11-15 17:22:42 +0100

    added initBoard function to SweGameState.java

--- src/main/java/swegame/state/SweGameState.java
+++ src/main/java/swegame/state/SweGameState.java
@@ -81,4 +81,16 @@ public class SweGameState implements Cloneable {
     }
 
 
+    private void initBoard(int[][] a) {
+        this.board = new Cell[5][4];
+        for (int i = 0; i < 5; ++i) {
+            for (int j = 0; j < 4; ++j) {
+                this.board[i][j] = Cell.of(a[i][j]);
+            }
+        }
+    }
+
+
+
+
 }
