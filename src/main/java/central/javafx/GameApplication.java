409d8cdca0f1e51c511a589956ec0f7703567fb4
Furjes-Benke Peter <fupn26@hotmail.hu>
2020-11-17 10:56:34 +0100

    changed window name in GameApplication.java

--- src/main/java/central/javafx/GameApplication.java
+++ src/main/java/central/javafx/GameApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,7 +14,7 @@ public class GameApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(GameApplication.class.getResource("/fxml/central/start.fxml"));
-        primaryStage.setTitle("Test Project");
+        primaryStage.setTitle("Premium Games");
         primaryStage.setResizable(false);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
