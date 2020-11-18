83000fad845284d4706959a23c2504473eefd35e
Fürjes-Benke Péter <fupn26@hotmail.hu>
2020-11-18 21:08:13 +0100

    added findLast function to GameResultDao in Dao game

--- src/main/java/daogame/data/GameResultDao.java
+++ src/main/java/daogame/data/GameResultDao.java
@@ -1,6 +1,9 @@
 package daogame.data;
 
 import central.util.jpa.GenericJpaDao;
+import com.google.inject.persist.Transactional;
+
+import java.util.List;
 
 /**
  * DAO class for the {@link daogame.data.GameResult} entity.
@@ -13,4 +16,18 @@ public class GameResultDao extends GenericJpaDao<GameResult> {
     public GameResultDao() {
         super(GameResult.class);
     }
+
+    /**
+     * Returns the list of {@code n} last results.
+     *
+     * @param n the maximum number of results to be returned
+     * @return the list of {@code n} last results
+     */
+    @Transactional
+    public List<GameResult> findLast(int n) {
+        return entityManager.createQuery("SELECT r FROM GameResult r ORDER BY r.created DESC ", daogame.data.GameResult.class)
+                .setMaxResults(n)
+                .getResultList();
+    }
+
 }
