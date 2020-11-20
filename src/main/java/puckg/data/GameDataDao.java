18b8707da83e29aeb4d33ee9188a5409da2e3f2b
Mezei Botond <botond20@gmail.com>
2020-11-20 21:59:23 +0100

    Added documentation to GameDataDao in puckg.data package

--- src/main/java/puckg/data/GameDataDao.java
+++ src/main/java/puckg/data/GameDataDao.java
@@ -6,12 +6,26 @@ import com.google.inject.persist.Transactional;
 import javax.persistence.Query;
 import java.util.List;
 
+/**
+ * DAO class for the {@link GameData} entity.
+ */
 public class GameDataDao extends GenericJpaDao<GameData> {
 
+    /**
+     * Constructor for the DAO class.
+     */
     public GameDataDao() {
         super(GameData.class);
     }
 
+    /**
+     * Returns the list of {@code n} best results with respect to the time
+     * spent for solving the puzzle.
+     *
+     * @param n the maximum number of results to be returned
+     * @return the list of {@code n} best results with respect to the time
+     * spent for solving the puzzle
+     */
     @Transactional
     public List<GameData> findBestByTime(int n) {
         return entityManager.createQuery("SELECT r FROM GameData r ORDER BY r.duration ASC, r.created DESC", GameData.class)
@@ -19,6 +33,14 @@ public class GameDataDao extends GenericJpaDao<GameData> {
                 .getResultList();
     }
 
+    /**
+     * Returns the list of {@code n} best results with respect to the winning
+     * player's final score.
+     *
+     * @param n the maximum number of results to be returned
+     * @return the list of {@code n} best results with respect to the winning
+     * player's final score
+     */
     @Transactional
     public List<GameData> findBestByPoint(int n) {
         return entityManager.createQuery("SELECT r FROM GameData r ORDER BY r.winnerPoints DESC, r.duration ASC", GameData.class)
