472475d3c39f9a6384affaa213e39e1fa6c5bc70
Mezei Botond <botond20@gmail.com>
2020-11-20 21:54:06 +0100

    Added documentation to GameData class in puckg.data package

--- src/main/java/puckg/data/GameData.java
+++ src/main/java/puckg/data/GameData.java
@@ -9,6 +9,9 @@ import javax.persistence.*;
 import java.time.Duration;
 import java.time.ZonedDateTime;
 
+/**
+ * Class representing the result of a game played by two specific players.
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -20,21 +23,39 @@ public class GameData {
     @GeneratedValue
     private long id;
 
+    /**
+     * The name of the winner of the game.
+     */
     @Column(nullable = false)
     private String winner;
 
+    /**
+     * The points of the winner of the game.
+     */
     @Column(nullable = false)
     private int winnerPoints;
 
+    /**
+     * The name of the second placed player.
+     */
     @Column(nullable = false)
     private String second;
 
+    /**
+     * The points of the second placed player.
+     */
     @Column(nullable = false)
     private int secondPoints;
 
+    /**
+     * The duration of the game.
+     */
     @Column(nullable = false)
     private Duration duration;
 
+    /**
+     * The timestamp when the result was saved.
+     */
     @Column(nullable = false)
     private ZonedDateTime created;
 
