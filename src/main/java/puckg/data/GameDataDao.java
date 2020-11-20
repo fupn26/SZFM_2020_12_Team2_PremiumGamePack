315265c32effcf7bd62cb3cc5fdf6b5a6c320e4d
Mezei Botond <botond20@gmail.com>
2020-11-20 21:57:11 +0100

    Added 3 queries to GameDataDao in puckg.data package

--- src/main/java/puckg/data/GameDataDao.java
+++ src/main/java/puckg/data/GameDataDao.java
@@ -1,4 +1,34 @@
 package puckg.data;
 
-public class GameDataDao {
+import central.util.jpa.GenericJpaDao;
+import com.google.inject.persist.Transactional;
+
+import javax.persistence.Query;
+import java.util.List;
+
+public class GameDataDao extends GenericJpaDao<GameData> {
+
+    public GameDataDao() {
+        super(GameData.class);
+    }
+
+    @Transactional
+    public List<GameData> findBestByTime(int n) {
+        return entityManager.createQuery("SELECT r FROM GameData r ORDER BY r.duration ASC, r.created DESC", GameData.class)
+                .setMaxResults(n)
+                .getResultList();
+    }
+
+    @Transactional
+    public List<GameData> findBestByPoint(int n) {
+        return entityManager.createQuery("SELECT r FROM GameData r ORDER BY r.winnerPoints DESC, r.duration ASC", GameData.class)
+                .setMaxResults(n)
+                .getResultList();
+    }
+
+    @Transactional
+    public void deleteData() {
+        Query query = entityManager.createQuery("DELETE FROM GameData");
+        query.executeUpdate();
+    }
 }
