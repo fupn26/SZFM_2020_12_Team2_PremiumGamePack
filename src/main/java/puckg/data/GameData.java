f67f7c6b11eb3c9282e3163ebd7aa91a82bf44b2
Mezei Botond <botond20@gmail.com>
2020-11-20 21:51:27 +0100

    Added variables with necessary annotations to GameData in puckg.data package

--- src/main/java/puckg/data/GameData.java
+++ src/main/java/puckg/data/GameData.java
@@ -1,4 +1,39 @@
 package puckg.data;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import java.time.Duration;
+import java.time.ZonedDateTime;
+
 public class GameData {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(nullable = false)
+    private String winner;
+
+    @Column(nullable = false)
+    private int winnerPoints;
+
+    @Column(nullable = false)
+    private String second;
+
+    @Column(nullable = false)
+    private int secondPoints;
+
+    @Column(nullable = false)
+    private Duration duration;
+
+    @Column(nullable = false)
+    private ZonedDateTime created;
+
+    @PrePersist
+    protected void onPersist() {
+        created = ZonedDateTime.now();
+    }
+
 }
