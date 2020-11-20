7a63ad2655f41f817ae5f4730378554591ae854a
Benedek <szabobenedek99@gmail.com>
2020-11-20 15:31:47 +0100

    fixed typo in GResultDao.java

--- src/main/java/swegame/data/GResultDao.java
+++ src/main/java/swegame/data/GResultDao.java
@@ -11,7 +11,7 @@ import java.util.List;
  */
 public class GResultDao extends GenericJpaDao<GResult> {
     /**
-     * Constructor for thew class.
+     * Constructor for the class.
      */
     public GResultDao() {
         super(GResult.class);
