59b248f70d2cb650236fa7448a3d0c451baa6e0b
Benedek <szabobenedek99@gmail.com>
2020-11-20 15:31:20 +0100

    added constructor to GResultDao in swegame

--- src/main/java/swegame/data/GResultDao.java
+++ src/main/java/swegame/data/GResultDao.java
@@ -1,4 +1,19 @@
 package swegame.data;
 
-public class GResultDao {
+import central.util.jpa.GenericJpaDao;
+import com.google.inject.persist.Transactional;
+
+import javax.persistence.Query;
+import java.util.List;
+
+/**
+ * DAO class for the {@link GResult} entity.
+ */
+public class GResultDao extends GenericJpaDao<GResult> {
+    /**
+     * Constructor for thew class.
+     */
+    public GResultDao() {
+        super(GResult.class);
+    }
 }
