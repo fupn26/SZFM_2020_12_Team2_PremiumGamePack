7f952f6a7ec293a4245dd58ae52c5e6e36187c21
Fürjes-Benke Péter <fupn26@hotmail.hu>
2020-11-21 13:49:34 +0100

    added execute function to GameResultJson class in Dao game

--- src/main/java/daogame/data/GameResultJson.java
+++ src/main/java/daogame/data/GameResultJson.java
@@ -1,4 +1,10 @@
 package daogame.data;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class GameResultJson {
+    public static void execute(GameResult newResult) throws IOException {
+    }
 }
