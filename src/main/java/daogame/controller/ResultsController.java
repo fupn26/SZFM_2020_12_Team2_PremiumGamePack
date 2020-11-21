a1e953b3941a824b1453783ca00475ebb5c2500c
Fürjes-Benke Péter <fupn26@hotmail.hu>
2020-11-21 16:52:20 +0100

    added necessary variables to ResultsController class in Dao game

--- src/main/java/daogame/controller/ResultsController.java
+++ src/main/java/daogame/controller/ResultsController.java
@@ -1,7 +1,62 @@
 package daogame.controller;
 
+import daogame.data.GameResult;
+import daogame.data.GameResultDao;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import lombok.extern.slf4j.Slf4j;
+
+import javax.inject.Inject;
+import java.time.Duration;
+import java.time.ZonedDateTime;
 
 @Slf4j
 public class ResultsController {
+    @Inject
+    private FXMLLoader fxmlLoader;
+
+    @Inject
+    private GameResultDao gameResultDao;
+
+    @FXML
+    private Label winnerLabel;
+
+    @FXML
+    private TableView resultTable;
+
+    @FXML
+    private TableView resultTable2;
+
+    @FXML
+    private TableColumn<GameResult, String> player1Column;
+
+    @FXML
+    private TableColumn<GameResult, String> player2Column;
+
+    @FXML
+    private TableColumn<GameResult, String> winnerColumn;
+
+    @FXML
+    private TableColumn<GameResult, Duration> durationColumn;
+
+    @FXML
+    private TableColumn<GameResult, ZonedDateTime> dateColumn;
+
+    @FXML
+    private TableColumn<GameResult, String> player1Column2;
+
+    @FXML
+    private TableColumn<GameResult, String> player2Column2;
+
+    @FXML
+    private TableColumn<GameResult, String> winnerColumn2;
+
+    @FXML
+    private TableColumn<GameResult, Duration> durationColumn2;
+
+    @FXML
+    private TableColumn<GameResult, ZonedDateTime> dateColumn2;
 }
