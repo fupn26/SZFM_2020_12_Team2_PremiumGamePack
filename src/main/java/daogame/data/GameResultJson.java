2d8d811b12f4c09e4d58ef475a25f3bed819041f
Fürjes-Benke Péter <fupn26@hotmail.hu>
2020-11-21 13:53:36 +0100

    added readJson function to GameresultJson class in Dao game

--- src/main/java/daogame/data/GameResultJson.java
+++ src/main/java/daogame/data/GameResultJson.java
@@ -1,10 +1,20 @@
 package daogame.data;
 
-import java.io.File;
-import java.io.IOException;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.*;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 public class GameResultJson {
+
     public static void execute(GameResult newResult) throws IOException {
+        //TODO handle new result
+    }
+
+    public static ArrayList<GameResult> readJson(File file) throws FileNotFoundException {
+        //TODO read data from json file in an Array list
+        return null;
     }
 }
