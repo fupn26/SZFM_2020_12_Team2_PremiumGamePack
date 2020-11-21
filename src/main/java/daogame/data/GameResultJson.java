eb33451300758c597769386402285b410b535233
Fürjes-Benke Péter <fupn26@hotmail.hu>
2020-11-21 13:55:49 +0100

    added getGson function to GameResultJson in Dao game for getting a Gson object with special serializer and deserializer

--- src/main/java/daogame/data/GameResultJson.java
+++ src/main/java/daogame/data/GameResultJson.java
@@ -1,10 +1,15 @@
 package daogame.data;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.TypeAdapter;
 import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
 
 import java.io.*;
 import java.lang.reflect.Type;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 
 public class GameResultJson {
@@ -16,5 +21,23 @@ public class GameResultJson {
     public static ArrayList<GameResult> readJson(File file) throws FileNotFoundException {
         //TODO read data from json file in an Array list
         return null;
+    }
+
+    public static Gson getGson() {
+        return new GsonBuilder()
+                .registerTypeAdapter(ZonedDateTime.class, new TypeAdapter<ZonedDateTime>() {
+                    @Override
+                    public void write(JsonWriter out, ZonedDateTime value) throws IOException {
+                        out.value(value.toString());
+                    }
+
+                    @Override
+                    public ZonedDateTime read(JsonReader in) throws IOException {
+                        return ZonedDateTime.parse(in.nextString());
+                    }
+                })
+                .enableComplexMapKeySerialization()
+                .setPrettyPrinting()
+                .create();
     }
 }
