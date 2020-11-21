c24a75a43bbaacc2e5621d30205b0ddfd6c971d6
Mezei Botond <botond20@gmail.com>
2020-11-21 11:43:21 +0100

    Added initialize function to puckg/HighScoreController

--- src/main/java/puckg/controller/HighScoreController.java
+++ src/main/java/puckg/controller/HighScoreController.java
@@ -1,16 +1,25 @@
 package puckg.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import org.apache.commons.lang3.time.DurationFormatUtils;
 import puckg.data.GameData;
 import puckg.data.GameDataDao;
 
 import javax.inject.Inject;
+import java.io.FileNotFoundException;
 import java.time.Duration;
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.List;
 
 public class HighScoreController {
 
@@ -70,4 +79,92 @@ public class HighScoreController {
 
     @FXML
     private Button mainMenuButton;
+
+    @FXML
+    private void initialize() {
+        List<GameData> highScoreList = gameDataDao.findBestByTime(30);
+
+        winner.setCellValueFactory(new PropertyValueFactory<>("winner"));
+        winnerPoints.setCellValueFactory(new PropertyValueFactory<>("winnerPoints"));
+        second.setCellValueFactory(new PropertyValueFactory<>("second"));
+        secondPoints.setCellValueFactory(new PropertyValueFactory<>("secondPoints"));
+        duration.setCellValueFactory(new PropertyValueFactory<>("duration"));
+        created.setCellValueFactory(new PropertyValueFactory<>("created"));
+
+        duration.setCellFactory(column -> {
+            TableCell<GameData, Duration> cell = new TableCell<GameData, Duration>() {
+                @Override
+                protected void updateItem(Duration item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if(empty) {
+                        setText(null);
+                    } else {
+                        setText(DurationFormatUtils.formatDuration(item.toMillis(),"H:mm:ss"));
+                    }
+                }
+            };
+            return cell;
+        });
+
+        created.setCellFactory(column -> {
+            TableCell<GameData, ZonedDateTime> cell = new TableCell<>() {
+                private DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM);
+
+                @Override
+                protected void updateItem(ZonedDateTime item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (empty) {
+                        setText(null);
+                    } else {
+                        setText(item.format(formatter));
+                    }
+                }
+            };
+            return cell;
+        });
+
+        winner2.setCellValueFactory(new PropertyValueFactory<>("winner"));
+        winnerPoints2.setCellValueFactory(new PropertyValueFactory<>("winnerPoints"));
+        second2.setCellValueFactory(new PropertyValueFactory<>("second"));
+        secondPoints2.setCellValueFactory(new PropertyValueFactory<>("secondPoints"));
+        duration2.setCellValueFactory(new PropertyValueFactory<>("duration"));
+        created2.setCellValueFactory(new PropertyValueFactory<>("created"));
+
+        duration2.setCellFactory(column -> {
+            TableCell<GameData, Duration> cell = new TableCell<GameData, Duration>() {
+                @Override
+                protected void updateItem(Duration item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if(empty) {
+                        setText(null);
+                    } else {
+                        setText(DurationFormatUtils.formatDuration(item.toMillis(),"H:mm:ss"));
+                    }
+                }
+            };
+            return cell;
+        });
+
+        created2.setCellFactory(column -> {
+            TableCell<GameData, ZonedDateTime> cell = new TableCell<>() {
+                private DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM);
+
+                @Override
+                protected void updateItem(ZonedDateTime item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (empty) {
+                        setText(null);
+                    } else {
+                        setText(item.format(formatter));
+                    }
+                }
+            };
+            return cell;
+        });
+
+        ObservableList<GameData> observableResult = FXCollections.observableArrayList();
+        observableResult.addAll(highScoreList);
+
+        highScoreTable.setItems(observableResult);
+    }
 }
