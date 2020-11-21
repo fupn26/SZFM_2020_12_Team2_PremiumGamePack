a2615ba56138248d91932e2287b06015e390ade1
Mezei Botond <botond20@gmail.com>
2020-11-21 11:51:40 +0100

    Added readJson function to GameDataJson in puckg

--- src/main/java/puckg/data/GameDataJson.java
+++ src/main/java/puckg/data/GameDataJson.java
@@ -3,11 +3,14 @@ package puckg.data;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.TypeAdapter;
+import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
 
-import java.io.IOException;
+import java.io.*;
+import java.lang.reflect.Type;
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
 
 public class GameDataJson {
 
@@ -27,5 +30,14 @@ public class GameDataJson {
                 .enableComplexMapKeySerialization()
                 .setPrettyPrinting()
                 .create();
+    }
+
+    public static ArrayList<GameData> readJson(File file) throws FileNotFoundException {
+
+        Reader reader = new FileReader(file);
+        Gson gson = getGson();
+        Type dataType = new TypeToken<ArrayList<GameData>>(){}.getType();
+
+        return gson.fromJson(reader, dataType);
     }
 }
