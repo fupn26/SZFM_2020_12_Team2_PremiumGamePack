5c9a448564a1ea76b60162a34abb30c421798eb6
Benedek <szabobenedek99@gmail.com>
2020-11-25 10:38:46 +0100

    added readJson method to GResultJson in triplepuck game

--- src/main/java/triplepuck/data/GResultJson.java
+++ src/main/java/triplepuck/data/GResultJson.java
@@ -1,4 +1,27 @@
 package triplepuck.data;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.TypeAdapter;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.*;
+import java.lang.reflect.Type;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class GResultJson {
+
+    public static ArrayList<GResult> readJson(File file) throws FileNotFoundException {
+
+        Reader reader = new FileReader(file);
+        Gson gson = getGson();
+        Type dataType = new TypeToken<ArrayList<GResult>>(){}.getType();
+
+        return gson.fromJson(reader, dataType);
+    }
 }
