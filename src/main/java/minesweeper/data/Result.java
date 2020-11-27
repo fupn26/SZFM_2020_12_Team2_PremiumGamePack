e727a0176aa9b5b116d4b19546401f2c63390baf
Balázs Bartha <balifej@gmail.com>
2020-11-27 22:51:47 +0100

    added minesweeper.data/Result

---
+++ src/main/java/minesweeper/data/Result.java
@@ -0,0 +1,55 @@
+package minesweeper.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.Duration;
+import java.time.ZonedDateTime;
+
+
+/**
+ * Class representing the result of a game.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Result {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    /**
+     * The name of the player.
+     */
+    @Column(nullable = false)
+    private String playerName;
+
+    /**
+     * Indicates if the game has been solved.
+     */
+    private boolean solved;
+
+    /**
+     * How long the game has taken.
+     */
+    @Column(nullable = false)
+    private Duration duration;
+
+    /**
+     * The time and date of when the result was saved.
+     */
+    @Column(nullable = false)
+    private ZonedDateTime created;
+
+    @PrePersist
+    protected void onPersist() {
+        created = ZonedDateTime.now();
+    }
+
+}
