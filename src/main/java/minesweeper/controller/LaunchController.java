30bf642b1738c2b4e5781e97277672ec1e939d71
Balázs Bartha <balifej@gmail.com>
2020-11-27 22:42:24 +0100

    added database initialization to minesweeper LaunchController

--- src/main/java/minesweeper/controller/LaunchController.java
+++ src/main/java/minesweeper/controller/LaunchController.java
@@ -3,6 +3,8 @@ package minesweeper.controller;
 import central.util.guice.PersistenceModule;
 import com.gluonhq.ignite.guice.GuiceContext;
 import com.google.inject.AbstractModule;
+import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.validation.RequiredFieldValidator;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -13,30 +15,48 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
+import puckg.data.GameDataDao;
 
 import javax.inject.Inject;
 import java.io.IOException;
 import java.util.List;
 
-
 @Slf4j
 public class LaunchController {
 
-    private FXMLLoader fxmlLoader = new FXMLLoader();
+    private GuiceContext context = new GuiceContext(this, () -> List.of(
+            new AbstractModule() {
+                @Override
+                protected void configure() {
+                    install(new PersistenceModule("minesweeper"));
+                    bind(GameDataDao.class);
+                }
+            }
+    ));
+
+    @Inject
+    private FXMLLoader fxmlLoader;
 
     @FXML
-    private TextField playerNameTextField;
-
-    @FXML
-    private Label errorLabel;
+    private JFXTextField playerNameTextField;
 
     @FXML
     public void initialize() {
+        context.init();
+
+        RequiredFieldValidator playerValidator = new RequiredFieldValidator();
+        playerValidator.setMessage("Input Required");
+
+        playerNameTextField.getValidators().add(playerValidator);
+
+        playerNameTextField.focusedProperty().addListener((o,oldVal,newVal)->{
+            if(!newVal) playerNameTextField.validate();
+        });
     }
 
     public void startAction(ActionEvent actionEvent) throws IOException {
-        if (playerNameTextField.getText().isEmpty()) {
-            errorLabel.setText("Enter your name!");
+        if (!playerNameTextField.validate()) {
+            return;
         } else {
             fxmlLoader.setLocation(getClass().getResource("/fxml/minesweeper/game.fxml"));
             Parent root = fxmlLoader.load();
@@ -46,4 +66,5 @@ public class LaunchController {
             stage.show();
         }
     }
+
 }
