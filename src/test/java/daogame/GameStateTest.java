d39f7f35cc17ba9403624f0e89955c95fa2fb74d
Fürjes-Benke Péter <fupn26@hotmail.hu>
2020-12-03 19:51:17 +0100

    added testOP function to GameStateTest.java

--- src/test/java/daogame/GameStateTest.java
+++ src/test/java/daogame/GameStateTest.java
@@ -1,4 +1,4 @@
-package state;
+package daogame.state;
 
 import org.junit.jupiter.api.Test;
 
@@ -6,4 +6,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GameStateTest {
 
+    @Test
+    void testOp() {
+        GameState state = new GameState();
+        Position from = new Position(0, 0);
+        Position to = new Position(0, 1);
+        int a = state.getActualState()[from.getRow()][from.getColumn()];
+        int b = state.getActualState()[to.getRow()][to.getColumn()];
+        state.op(from, to);
+
+        assertEquals(b, state.getActualState()[from.getRow()][from.getColumn()]);
+        assertEquals(a, state.getActualState()[to.getRow()][to.getColumn()]);
+    }
 }
