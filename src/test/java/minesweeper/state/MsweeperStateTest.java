6978a725b082f44cfb838bf23fe8409467c4511a
Balázs Bartha <balifej@gmail.com>
2020-12-05 16:34:22 +0100

    implemented testThreeArgConstructor_InvalidArg

--- src/test/java/minesweeper/state/MsweeperStateTest.java
+++ src/test/java/minesweeper/state/MsweeperStateTest.java
@@ -7,6 +7,9 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MsweeperStateTest {
     @Test
     void testThreeArgConstructor_InvalidArg() {
+        assertThrows(IllegalArgumentException.class, () -> new MsweeperState(0,1,0));
+        assertThrows(IllegalArgumentException.class, () -> new MsweeperState(3,4,13));
+        assertThrows(IllegalArgumentException.class, () -> new MsweeperState(-1,-13, 10));
     }
 
     @Test
