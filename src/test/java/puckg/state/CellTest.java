00eee7794869f370843e708f961887d94c2362f6
Mezei Botond <botond20@gmail.com>
2020-12-05 14:52:03 +0100

    Added test of Cell.of() function to puckg/CellTest

--- src/test/java/puckg/state/CellTest.java
+++ src/test/java/puckg/state/CellTest.java
@@ -1,8 +1,19 @@
 package java.puckg.state;
 
 import org.junit.jupiter.api.Test;
+import puckg.state.Cell;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CellTest {
+
+    @Test
+    void testOf() {
+        assertEquals(Cell.EMPTY, Cell.of(0));
+        assertEquals(Cell.RED, Cell.of(1));
+        assertEquals(Cell.BLUE, Cell.of(2));
+        assertEquals(Cell.BLACK, Cell.of(3));
+        assertThrows(IllegalArgumentException.class, () -> Cell.of(4));
+        assertThrows(IllegalArgumentException.class, () -> Cell.of(-1));
+    }
 }
