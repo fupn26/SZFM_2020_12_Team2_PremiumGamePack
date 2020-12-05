e5133f0127a892868cd6f85fb77fff7284ea9203
Mezei Botond <botond20@gmail.com>
2020-12-05 15:05:14 +0100

    Added constructor test to puckg/TableStateTest

--- src/test/java/puckg/state/TableStateTest.java
+++ src/test/java/puckg/state/TableStateTest.java
@@ -1,4 +1,19 @@
 package puckg.state;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class TableStateTest {
+
+    @Test
+    void testConstructor() {
+        assertThrows(IllegalArgumentException.class, () -> new TableState(new int[][] {
+                {2, 2, 0, 0, 0, 2, 0},
+                {0, 2, 0, 0, 2, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 3, 0, 0, 0},
+                {0, 0, 0, 0, 2, 2, 0},
+                {2, 0, 0, 0, 2, 2, 0}}));
+    }
 }
