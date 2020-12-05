08ca450ee36c702588360535fe6e26b1682171ec
Fürjes-Benke Péter <fupn26@hotmail.hu>
2020-12-05 10:30:34 +0100

    added testIsApplicable function to GameStateTest.java

--- src/test/java/daogame/GameStateTest.java
+++ src/test/java/daogame/GameStateTest.java
@@ -18,4 +18,22 @@ class GameStateTest {
         assertEquals(b, state.getActualState()[from.getRow()][from.getColumn()]);
         assertEquals(a, state.getActualState()[to.getRow()][to.getColumn()]);
     }
+
+    @Test
+    void testIsApplicable() {
+        GameState state = new GameState(new int[][]{{2, 0, 0, 1}, {0, 2, 1, 0}, {0, 1, 2, 0}, {1, 0, 0, 2}},2);
+
+        assertFalse(state.isApplicable(new Position(0, 3), new Position(0, 2)));
+        assertFalse(state.isApplicable(new Position(0, 0), new Position(0, 3)));
+        assertFalse(state.isApplicable(new Position(0, 0), new Position(1, 3)));
+        assertTrue(state.isApplicable(new Position(0, 0), new Position(0, 1)));
+        assertTrue(state.isApplicable(new Position(0, 0), new Position(1, 0)));
+        assertTrue(state.isApplicable(new Position(1, 1), new Position(1, 0)));
+        assertTrue(state.isApplicable(new Position(1, 1), new Position(0, 2)));
+        assertTrue(state.isApplicable(new Position(1,1), new Position(2, 0)));
+        assertTrue(state.isApplicable(new Position(1, 1), new Position(0, 1)));
+
+        state = new GameState(new int[][]{{2, 0, 0, 1}, {0, 2, 1, 0}, {0, 1, 0, 2}, {1, 0, 0, 2}}, 2);
+        assertFalse(state.isApplicable(new Position(0, 0), new Position(2, 2)));
+    }
 }
