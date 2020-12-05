e2a4cb66afbe64cae465e1eb937ed788514799ea
Mezei Botond <botond20@gmail.com>
2020-12-05 15:08:08 +0100

    Added test of isMoveAvailable() function to puckg/TableStateTest

--- src/test/java/puckg/state/TableStateTest.java
+++ src/test/java/puckg/state/TableStateTest.java
@@ -78,4 +78,65 @@ public class TableStateTest {
                 {2, 0, 0, 0, 2, 2}}).isFinished(1));
 
     }
+
+    @Test
+    void testIsMoveAvailable() {
+        assertTrue(new TableState().isMoveAvailable(0, 0));
+        assertTrue(new TableState(new int[][] {
+                {2, 2, 0, 0, 0, 2},
+                {0, 2, 0, 0, 2, 0},
+                {0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 3, 0, 2},
+                {0, 0, 0, 0, 2, 2},
+                {2, 0, 0, 0, 2, 1}}).isMoveAvailable(5, 5));
+        assertTrue(new TableState(new int[][] {
+                {2, 2, 0, 0, 0, 2},
+                {0, 2, 0, 0, 2, 0},
+                {0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 3, 0, 0},
+                {0, 0, 0, 0, 2, 2},
+                {2, 0, 0, 2, 2, 1}}).isMoveAvailable(5, 5));
+        assertTrue(new TableState(new int[][] {
+                {2, 2, 0, 0, 0, 2},
+                {0, 2, 0, 0, 2, 0},
+                {0, 0, 0, 0, 2, 0},
+                {0, 0, 0, 3, 0, 2},
+                {0, 0, 2, 0, 1, 2},
+                {2, 0, 0, 2, 2, 2}}).isMoveAvailable(4, 4));
+        assertFalse(new TableState(new int[][] {
+                {2, 2, 0, 0, 0, 2},
+                {0, 2, 0, 0, 2, 0},
+                {0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 3, 0, 2},
+                {0, 0, 0, 0, 2, 2},
+                {2, 0, 0, 2, 2, 1}}).isMoveAvailable(5, 5));
+        assertTrue(new TableState(new int[][] {
+                {2, 2, 2, 0, 0, 2},
+                {0, 2, 2, 0, 2, 0},
+                {2, 0, 1, 0, 2, 0},
+                {0, 0, 0, 3, 0, 2},
+                {2, 0, 2, 0, 2, 2},
+                {2, 0, 0, 2, 2, 2}}).isMoveAvailable(2, 2));
+        assertTrue(new TableState(new int[][] {
+                {2, 2, 2, 0, 2, 2},
+                {0, 2, 2, 0, 2, 0},
+                {2, 0, 1, 0, 2, 0},
+                {0, 0, 0, 3, 0, 2},
+                {0, 0, 2, 0, 2, 2},
+                {2, 0, 0, 2, 2, 2}}).isMoveAvailable(2, 2));
+        assertTrue(new TableState(new int[][] {
+                {2, 2, 2, 0, 2, 2},
+                {0, 2, 2, 0, 2, 0},
+                {2, 0, 1, 0, 2, 0},
+                {0, 0, 0, 3, 0, 2},
+                {2, 0, 2, 0, 0, 2},
+                {2, 0, 0, 2, 2, 2}}).isMoveAvailable(2, 2));
+        assertTrue(new TableState(new int[][] {
+                {2, 2, 2, 0, 2, 2},
+                {0, 2, 2, 0, 2, 0},
+                {2, 0, 1, 0, 0, 0},
+                {0, 0, 0, 3, 0, 2},
+                {2, 0, 2, 0, 2, 2},
+                {2, 0, 0, 2, 2, 2}}).isMoveAvailable(2, 2));
+    }
 }
