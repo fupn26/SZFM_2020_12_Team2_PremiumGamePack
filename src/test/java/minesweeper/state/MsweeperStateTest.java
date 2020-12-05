29e9f6d06883f6f6c1d2357a962a5fc9c26424be
Balázs Bartha <balifej@gmail.com>
2020-12-05 16:33:18 +0100

    added testOneArgConstructor_InvalidArg

--- src/test/java/minesweeper/state/MsweeperStateTest.java
+++ src/test/java/minesweeper/state/MsweeperStateTest.java
@@ -6,11 +6,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class MsweeperStateTest {
     @Test
-    void testThreeArgConstructor_InvalidArg(){
+    void testThreeArgConstructor_InvalidArg() {
     }
 
     @Test
-    void testThreeArgConstructor_ValidArg(){
+    void testThreeArgConstructor_ValidArg() {
     }
 
+    @Test
+    void testOneArgConstructor_InvalidArg() {
+    }
 }
